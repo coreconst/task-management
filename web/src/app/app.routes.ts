8c0a71c9ae2230528710ec95915db0d4e896af9c
constantine <parfilokostya0@gmail.com>
2026-02-08 18:23:37 +0200

    Add auth login/signup pages

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login';
+import { SignupComponent } from './auth/signup/signup';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '**', redirectTo: 'login' }
+];
