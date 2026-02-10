99ab4c38ec380eb97c3992b124d6e7c7e0ab06e9
constantine <parfilokostya0@gmail.com>
2026-02-10 23:18:22 +0200

    Add register route to web signup

--- web/src/app/auth/signup/signup.ts
+++ web/src/app/auth/signup/signup.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.css'
 })
-export class SignupComponent {}
+export class SignupComponent {
+  private readonly apiUrl: string = 'api/auth/register';
+
+  form = {
+    name: '',
+    email: '',
+    password: '',
+  };
+
+  statusMessage = '';
+
+  constructor(private readonly http: HttpClient) {}
+
+  submit() {
+    this.statusMessage = '';
+
+    this.http.post(this.apiUrl, this.form).subscribe({
+      next: () => {
+        this.statusMessage = 'Account created successfully.';
+        this.form = { name: '', email: '', password: '' };
+      },
+      error: () => {
+        this.statusMessage = 'Failed to create account. Try again.';
+      },
+    });
+  }
+}
