3a547c0d62dc8616445f7f7d431c21da9ac0aa53
constantine <parfilokostya0@gmail.com>
2026-02-10 23:16:18 +0200

    Add user module with schema

---
+++ api/src/modules/user/dto/update-user.dto.ts
@@ -0,0 +1,5 @@
+export class UpdateUserDto {
+  email?: string;
+  passwordHash?: string;
+  name?: string;
+}
---
+++ api/src/modules/user/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import {User, UserDocument} from './schemas/user.schema';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<UserDocument> {
+    const user = new this.userModel(createUserDto);
+    return user.save();
+  }
+
+  async findOne(id: string): Promise<UserDocument | null> {
+    return this.userModel.findById(id).exec();
+  }
+
+  async findByEmail(email: string): Promise<UserDocument | null> {
+    return this.userModel.findOne({ email }).exec();
+  }
+
+  async findAll(): Promise<UserDocument[]> {
+    return this.userModel.find().exec();
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<UserDocument | null> {
+    return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true }).exec();
+  }
+}
