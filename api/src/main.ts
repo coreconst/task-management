3520fcf6f2bdc9ce8a0adb7d8634715b6603beef
constantine <parfilokostya0@gmail.com>
2026-02-10 23:14:24 +0200

    Add necessary package and setup main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: [configService.get<string>('WEB_URL') || ''],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
   await app.listen(3000);
 }
 bootstrap();
